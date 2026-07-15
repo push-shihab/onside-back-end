748170c91adaec03cff0a45b99c6fdd3ce0ac330
push-shihab <shihabfamily539@gmail.com>
2026-07-16 00:00:14 +0600

    ready to deploy in vercel

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const db = client.db("onside");
     const allPlayers = db.collection("players");
     const sessionCollection = db.collection("session");
@@ -124,7 +124,7 @@ async function run() {
       },
     );
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
